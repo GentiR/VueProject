09b50521cbabc31bfb5bac459efffc5245d6d5e9
BlerinaRestelica <94801584+BlerinaRestelicaa@users.noreply.github.com>
2022-02-14 16:54:42 +0100

    Adding Profile Menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -13,6 +13,40 @@
           <router-link class="link" to="#"> Create Post </router-link>
           <router-link class="link" :to="{name: 'Login'}"> Login/Register </router-link>
         </ul>
+        <div v-if="user" @click="toggleProfileMenu" class="profile" ref="profile">
+          <!-- referencing our profile initials form our state -->
+          <span>{{ this.$store.state.profileInitials }}</span>
+          <div v-show="profileMenu" class="profile-menu">
+            <div class="info">
+              <p class="initials">{{ this.$store.state.profileInitial }}</p>
+              <div class="right">
+                <p>{{ this.$store.state.profileFirstName }} {{ this.$store.state.profileLastName }}</p>
+                <p>{{ this.$store.state.profileUsername }}</p>
+                <p>{{ this.$store.state.profileEmail }}</p>
+              </div>
+            </div>
+            <div class="options">
+              <div class="option">
+                <router-link class="option" to="#">
+                  <userIcon class="icon"/>
+                  <p>Profile</p>
+                </router-link>
+              </div>
+               <div class="option">
+                <router-link class="option" to="#">
+                  <adminIcon class="icon"/>
+                  <p>Admin</p>
+                </router-link>
+              </div>
+               <div @click="signOut" class="option">
+               
+                  <signOutIcon class="icon"/>
+                  <p>Sign Out</p>
+              
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
     <!-- this nav bar is needed for mobile app -->
@@ -31,14 +65,22 @@
 <script>
 //import { component } from 'vue/types/umd';
 import menuIcon from "../assets/Icons/bars-regular.svg";
-
+import userIcon from "../assets/Icons/user-alt-light.svg";
+import adminIcon from "../assets/Icons/user-crown-light.svg";
+import signOutIcon from "../assets/Icons/sign-out-alt-regular.svg";
+import firebase from "firebase/app";
+import "firebase/auth";
 export default {
   name: "navigation",
   components: {
     menuIcon,
+    userIcon,
+    adminIcon,
+    signOutIcon,
   },
   data() {
     return {
+      profileMenu: null,
       mobile: null,
       mobileNav: null,
       windownWidth: null,
@@ -63,6 +105,22 @@ export default {
 
     toogleMobileNav() {
       this.mobileNav = !this.mobileNav;
+    },
+
+    toggleProfileMenu(e){
+      if(e.target === this.$refs.profile){
+         this.profileMenu = !this.profileMenu;
+      }
+      
+    },
+    signOut(){
+      firebase.auth().signOut();
+      window.location.reload();
+    }
+  },
+  computed:{
+    user(){
+      return this.$store.state.user;
     },
   },
 };
@@ -120,6 +178,85 @@ header {
           margin-right: 0;
         }
       }
+     .profile{
+       position: relative;
+       cursor: pointer;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+       width: 40px;
+       height: 40px;
+       border-radius: 50%;
+       color: white;
+       background-color:#303030 ;
+
+       span{
+         pointer-events: none;
+       }
+
+       .profile-menu{
+         position: absolute;
+         top: 60px;
+         right: 0;
+         width: 250px;
+         background-color: #303030;
+         box-shadow:  4px 6px -1px rgba(0, 0, 0, 0.1) , 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+         .info{
+           display: flex;
+           align-items: center;
+           padding: 15px;
+           border-bottom: 1px solid white;
+
+           .initials{
+             position: initial;
+             width: 40px;
+             height: 40px;
+             background-color: white;
+             color: #303030;
+             display: flex;
+             align-items: center;
+             justify-content: center;
+             border-radius: 50%;
+           }
+           .right{
+             flex: 1;
+             margin-left: 24px;
+
+             p:nth-child(1){
+               font-size: 14px;
+             }
+             p:nth-child(2),
+             p:nth-child(3){
+               font-size: 12px;
+             }
+           }
+         }
+         .options{
+           padding: 15px;
+           .option{
+             text-decoration: none;
+             color: white;
+             display: flex;
+             align-items: center;
+             margin-bottom: 12px;
+
+             .icon{
+               width: 18px;
+               height: auto;
+             }
+             p{
+               font-size:14px;
+               margin-left: 12px;
+               }
+             }
+             .option:last-child{
+               margin-bottom: 0px ;
+             }
+           }
+         }
+       }
+     } 
     }
   }
 
@@ -161,5 +298,5 @@ header {
   .mobile-nav-leave-to {
     transform: translateX(-250px);
   }
-}
+
 </style>
