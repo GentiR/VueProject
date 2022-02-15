7b8e2df7ba376f670780623e355b60b5e3f4b543
BlerinaRestelica <94801584+BlerinaRestelicaa@users.noreply.github.com>
2022-02-15 16:50:32 +0100

    Update Admin.vue

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,42 +1,83 @@
 <template>
-  <div class="profile">
-      <Modal v-if="modalActive" :modalMessage="modalMessage" v-on:close-modal="closeModal"/>
-      <div class="container">
-          <h2>Account Settings</h2>
-          <div class="profile-info">
-              <div class="initals">{{ $store.state.profileInitials }}</div>
-              <div class="admin-badge">
-                  <adminIcon class="icon"/>
-                  <span>admin</span>
-              </div>
-              <div class="input">
-                  <label for="firstName"> First Name</label>
-                  <input type="text" id="firstName" v-model="firstName"/>
-              </div>
-              <div class="input">
-                  <label for="lastName"> Last Name</label>
-                  <input type="text" id="lastName" v-model="lastName"/>
-              </div>
-              <div class="input">
-                  <label for="username"> Username</label>
-                  <input type="text" id="username" v-model="username"/>
-              </div>
-              <div class="input">
-                  <label for="email"> Email</label>
-                  <input disabled type="text" id="email" v-model="email"/>
-              </div>
-              <button>Save Changes</button>
-          </div>
-      </div>
-  </div>
+ <div class="admin">
+     <div class="container">
+         <h2>Administration</h2>
+         <div class="admin-info">
+             <h2>Add Admin</h2>
+             <div class="input">
+                 <input placeholder="Enter a user email to make them admin" type="text" id="addAdmins" v-model="adminEmail" />
+             </div>
+             <span>{{ this.functionMsg }}</span>
+             <button class="button"> Submit</button>
+         </div>
+     </div>
+ </div>
 </template>
 
 <script>
 export default {
-
+name: "Admin",
+data(){
+    return{
+        adminEmail: "",
+        functionMsg: null,
+    };
+},
 };
 </script>
 
 <style lang="scss" scoped>
+.admin{
+    .container{
+    max-width: 1000px;
+    padding: 60px 25px;
 
+    h2{
+        text-align: center;
+        margin-bottom: 16px;
+        font-weight: 300;
+        font-size: 32px;
+    }
+
+    .admin-info{
+        border-radius: 10px;
+        box-shadow:  4px 6px -1px rgba(0, 0, 0, 0.1) , 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        padding: 32px;
+        background-color:#d0f0c0a8;
+        display: flex;
+        flex-direction: column;
+        max-width: 600px;
+        margin: 32px auto;
+
+        span{
+            font-size: 14px;
+        }
+        .input{
+            margin: 16px 0;
+
+            label{
+                font-size: 14px;
+                display: block;
+                padding-bottom: 6px;
+            }
+            input{
+                width: 100%;
+                border: none;
+                background-color: white;
+                padding: 8px;
+                height: 50px;
+                @media (max-width: 900px) {
+                    
+                }
+                &:focus{
+                    outline: none;
+                }
+            }
+        }
+        button{
+            align-self: center;
+        }
+    }
+  }
+}
 </style>
