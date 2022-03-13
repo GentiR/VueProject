657b7d121b72a36796bd195df6d03206dfcee84e
Gent Rechica <gr48119@ubt-uni.net>
2022-03-13 18:52:38 +0100

    Updating PostCreate

--- src/router/index.js
+++ src/router/index.js
@@ -12,89 +12,88 @@ import BlogPreview from "../views/BlogPreview.vue";
 import ViewBlog from "../views/ViewBlog.vue";
 
 Vue.use(VueRouter);
-//routes of our components
+
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
-    meta:{
-      title: 'Home'
-    }
+    meta: {
+      title: "Home",
+    },
   },
   {
     path: "/blogs",
     name: "Blogs",
     component: Blogs,
-    meta:{
-      title: 'Blogs'
-    }
+    meta: {
+      title: "Blogs",
+    },
   },
   {
     path: "/login",
     name: "Login",
     component: Login,
-    meta:{
-      title: 'Login'
-    }
+    meta: {
+      title: "Login",
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
-    meta:{
-      title: 'Register'
-    }
+    meta: {
+      title: "Register",
+    },
   },
   {
     path: "/forgot-password",
     name: "ForgotPassword",
     component: ForgotPassword,
-    meta:{
-      title: 'Forgot Password'
+    meta: {
+      title: "Forgot Password",
     },
   },
   {
     path: "/profile",
     name: "Profile",
     component: Profile,
-    meta:{
-      title: 'Profile'
+    meta: {
+      title: "Profile",
     },
   },
   {
     path: "/admin",
     name: "Admin",
     component: Admin,
-    meta:{
-      title: 'Admin'
+    meta: {
+      title: "Admin",
     },
   },
   {
     path: "/create-post",
     name: "CreatePost",
     component: CreatePost,
-    meta:{
-      title: 'Create Post'
+    meta: {
+      title: "Create Post",
     },
   },
   {
     path: "/post-preview",
     name: "BlogPreview",
     component: BlogPreview,
-    meta:{
-      title: 'Preview Post'
+    meta: {
+      title: "Preview Blog Post",
     },
   },
   {
-    path: "/view-blog/:blogid",
+    path: "/view-blog",
     name: "ViewBlog",
     component: ViewBlog,
-    meta:{
-      title: 'View Blog Post'
+    meta: {
+      title: "View Blog Post",
     },
   },
-  
 ];
 
 const router = new VueRouter({
@@ -102,10 +101,10 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
-//navigating the documet titles
-router.beforeEach((to, from, next) =>{
-document.title =  to.meta.title + '| FoodStories';
-next();
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | FoodStories`;
+  next();
 });
 
 export default router;
